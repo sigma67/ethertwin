7af58179b779f8995c21b1d95c531d57b4d8dd47
Benedikt <benedikt.putz@ur.de>
2020-04-03 20:09:18 +0200

    fix run script

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
- nohup ganache-cli -p 7545 -e 500000 -h "0.0.0.0" - -i 66 -m "athlete thought wild chef tennis skirt horror always advice distance donate strike" --db ganache &> ganache.log &
- nohup node agent.js &> agent.log &
+nohup ganache-cli -p 7545 -e 500000 -h "0.0.0.0" - -i 66 -m "athlete thought wild chef tennis skirt horror always advice distance donate strike" --db ganache > ganache.log 2>&1&
+sleep 5
+nohup node agent.js > agent.log 2>&1&
