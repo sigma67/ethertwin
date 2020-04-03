1618d81aef94087a80e5b993cb26a159de3b5ad4
sigma67 <benedikt.putz@wiwi.uni-regensburg.de>
2020-04-03 18:53:23 +0200

    Changes to run agent on Pi

---
+++ run.sh
@@ -0,0 +1,2 @@
+ nohup ganache-cli -p 7545 -e 500000 -h "0.0.0.0" - -i 66 -m "athlete thought wild chef tennis skirt horror always advice distance donate strike" --db ganache &> ganache.log &
+ nohup node agent.js &> agent.log &
